72c12fb14fdfcc8b61a311b125ee5b4d0c802b82
kdrl <admin@kdrl.local>
2016-08-09 12:30:27 +0900

    wana service

--- minami/edison/wana.js
+++ minami/edison/wana.js
@@ -6,31 +6,30 @@ var InPin = new mraa.Aio(0);
 OutPin.dir(mraa.DIR_OUT);
 OutPin.write(1);
 
-var isConnected = (InPin.read()>600?1:0);
+var isConnected = (InPin.read()>600?true:false);
 
 setInterval(function () {//これより1秒間隔でセンサーの値を取ってくることとなる
-  isConnected = (InPin.read()>600?1:0);
+  isConnected = (InPin.read()>600?true:false);
   console.log(isConnected)
 }, 1000);
 
-// var socket = require('socket.io-client')('http://192.168.0.23:3000', { query: "edisonCode="+edisonCode });
+var socket = require('socket.io-client')('http://192.168.0.23:3000', { query: "edisonCode="+edisonCode });
 
-// socket.on('connect',function(){ 
-//     console.log("connected")
-//     sendData();
-// });
+socket.on('connect',function(){ 
+    console.log("connected")
+    sendData();
+});
 
-// function sendData(){
-// 	console.log("send Data");
+function sendData(){
+	console.log("send Data");
 
-// 	var dt    = new Date();// 現在時刻の取得
-// 	dt.setTime(dt.getTime() + 32400000); // 1000 * 60 * 60 * 9(hour)	// 日本の時間に修正
+	var dt    = new Date();// 現在時刻の取得
+	dt.setTime(dt.getTime() + 32400000); // 1000 * 60 * 60 * 9(hour)	// 日本の時間に修正
 
-// 	socket.emit('sendData', {
-// 		edisonCode:edisonCode,
-// 		Date : dt,
-// 		Light : Light,
-// 	 	Temp : Temp 
-// 	});
-// 	setTimeout(sendData,5000);//5秒に一回送信する(５秒間の平均を送るように工夫を入れたい)
-// }
+	socket.emit('sendData', {
+		edisonCode:edisonCode,
+		Date : dt,
+		isConnected: isConnected
+	});
+	setTimeout(sendData,5000);//5秒に一回送信する(５秒間の平均を送るように工夫を入れたい)
+}
