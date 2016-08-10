af9499f6571aef4ba91d2a7c0b333df569e80833
kdrl <kdrl7@naver.com>
2016-08-10 22:06:12 +0000

    all

--- minami/edison/testservo4.js
+++ minami/edison/testservo4.js
@@ -2,10 +2,11 @@
 var servoModule = require("jsupm_servo");
 
 //Instantiate ES08A Servo module on GPIO 5
-var servo1 = new servoModule.ES08A(7);
-var servo2 = new servoModule.ES08A(8);
-var servo3 = new servoModule.ES08A(9);
-var servo4 = new servoModule.ES08A(10);
+var servo1 = new servoModule.ES08A(5);
+//var servo2 = new servoModule.ES08A(8);
+//var servo3 = new servoModule.ES08A(9);
+//var servo4 = new servoModule.ES08A(10);
+console.dir(servoModule)
 // function to initialize servo
     // timeOffset: how long after hitting "run"
     //		should we start this servo instance
@@ -31,7 +32,7 @@ function startServo(timeOffset, timeInterval, angle)
     // angle is passed as a param to the specified function
 }
 // start immediately, run every 3 seconds, go 0 degrees
-startServo(0, 3000, 0);
+//startServo(0, 3000, 0);
 
 // // start in 1 second, run every 3 seconds, go 90 degrees
 // startServo(1000, 3000, 90);
