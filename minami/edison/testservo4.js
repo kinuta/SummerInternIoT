9b24b9bd83a6e3d09ee735fcf10a6c101f184e2a
kdrl <admin@kdrl.local>
2016-08-11 06:59:46 +0900

    robot

--- minami/edison/testservo4.js
+++ minami/edison/testservo4.js
@@ -2,13 +2,14 @@
 var servoModule = require("jsupm_servo");
 
 //Instantiate ES08A Servo module on GPIO 5
-var servo = new servoModule.ES08A(5);
-
+var servo1 = new servoModule.ES08A(7);
+var servo2 = new servoModule.ES08A(8);
+var servo3 = new servoModule.ES08A(9);
+var servo4 = new servoModule.ES08A(10);
 // function to initialize servo
     // timeOffset: how long after hitting "run"
     //		should we start this servo instance
-    // timeInterval: how frequently should this instance run after 
-timeOffset
+    // timeInterval: how frequently should this instance run after timeOffset
     // angle: the angle for this instance
 function startServo(timeOffset, timeInterval, angle)
 {
@@ -18,7 +19,10 @@ function startServo(timeOffset, timeInterval, angle)
         // run this instance every timeInterval milliseconds
         setInterval(function()
         {
-            servo.setAngle(angle);
+            servo1.setAngle(angle);
+            servo2.setAngle(angle);
+            servo3.setAngle(angle);
+            servo4.setAngle(angle);
             console.log("Set angle to " + angle);
         }, timeInterval);
     }, timeOffset);
@@ -29,11 +33,11 @@ function startServo(timeOffset, timeInterval, angle)
 // start immediately, run every 3 seconds, go 0 degrees
 startServo(0, 3000, 0);
 
-// start in 1 second, run every 3 seconds, go 90 degrees
-startServo(1000, 3000, 90);
+// // start in 1 second, run every 3 seconds, go 90 degrees
+// startServo(1000, 3000, 90);
 
-// start in 2 seconds, run every 3 seconds, go 180 degrees
-startServo(2000, 3000, 180);
+// // start in 2 seconds, run every 3 seconds, go 180 degrees
+// startServo(2000, 3000, 180);
 
 // Print message when exiting
 process.on('SIGINT', function()
