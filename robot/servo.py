67531ca10906c10492b5202149e92aa75d56be81
kdrl <admin@kdrl.local>
2016-08-12 05:11:26 +0900

    servo.py

--- robot/servo.py
+++ robot/servo.py
@@ -1,24 +1,17 @@
 import time
+import random
 import pyupm_servo as servo 
 
 # Create the servo 
 gServo = servo.ES08A(5)
 
-for i in range(0,1000): 
-    # Set the servo arm to 0 degrees
-    gServo.setAngle(0)
-    print 'Set angle to 0'
-    time.sleep(1)
+def getNextAngle():
+    return random.randint(0, 180)
 
-    # Set the servo arm to 90 degrees
-    gServo.setAngle(90)
-    print 'Set angle to 90'
-    time.sleep(1)
-
-    # Set the servo arm to 180 degrees
-    gServo.setAngle(180)
-    print 'Set angle to 180'
-    time.sleep(1)
+def gogogo(angle):
+    gServo.setAngle(angle)
+    time.sleep(0.5)
+    return gogogo(getNextAngle())
 
 # Delete the servo object
 del gServo 
