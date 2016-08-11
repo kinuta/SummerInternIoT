4cf6f201f37eb47ad2ad3c843d6ab8610028a1d7
kdrl <admin@kdrl.local>
2016-08-12 06:01:07 +0900

    update

--- robot/servo.js
+++ robot/servo.js
@@ -1,22 +1,24 @@
 var servoModule = require("jsupm_servo");
 
-var servo = new servoModule.ES08A(5);
+var servo1 = new servoModule.ES08A(5);
+var servo2 = new servoModule.ES08A(6);
 
 var timeOffSet = 500;
 
-function setServoAngle(angle){
+function setServoAngle(servo,angle){
 	servo.setAngle(angle);
 	console.log("Set angle to " + angle);
 	var nextAngle = Math.floor(Math.random()*180);
 	setTimeout(function(){
-		setServoAngle(nextAngle);
+		setServoAngle(servo,nextAngle);
 	},timeOffSet);
 }
 
 console.log("start simasu get ready~")
 
 setTimeout(function(){
-		setServoAngle(0);
+		setServoAngle(servo1,0);
+		setServoAngle(servo2,0);
 },1000);
 
 process.on('SIGINT', function()
