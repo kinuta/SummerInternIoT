2152c430b29683c9566754c07c74c83e5fdcf197
kdrl <admin@kdrl.local>
2016-08-12 08:50:07 +0900

    test

---
+++ robot/robo.py
@@ -0,0 +1,41 @@
+from time import sleep
+import pyupm_ttp223 as ttp223
+import pyupm_grove as grove
+import pyupm_servo as servo 
+
+servo1Rotary = grove.GroveRotary(0)
+servo2Rotary = grove.GroveRotary(1)
+servo3Rotary = grove.GroveRotary(2)
+servo4Rotary = grove.GroveRotary(3)
+
+servo5Touch = ttp223.TTP223(4)
+
+servo1 = servo.ES08A(5)#したのやつ
+servo2 = servo.ES08A(6)
+servo3 = servo.ES08A(7)
+servo4 = servo.ES08A(8)
+servo5 = servo.ES08A(9)#上のやつ。つまりここが手
+
+while True:
+
+    servo1Angle = int(servo1Rotary.abs_value())*180/1024
+    servo2Angle = int(servo2Rotary.abs_value())*180/1024
+    servo3Angle = int(servo3Rotary.abs_value())*180/1024
+    servo4Angle = int(servo4Rotary.abs_value())*180/1024
+
+    servo5Activate = servo5Touch.isPressed()
+
+    print "servo1angle: %4d" % servo1angle, "servo2angle: %4d" % servo2angle, "servo3angle: %4d" % servo3angle, "servo4angle: %4d" % servo4angle, "servo5Activate: %r" % servo5Activate 
+
+	servo1.setAngle(servo1Angle)
+	servo2.setAngle(servo2Angle)
+	servo3.setAngle(servo3Angle)
+	servo4.setAngle(servo4Angle)
+	if(servo5Activate){
+		servo5.setAngle(180)
+	}else{
+		servo5.setAngle(0)
+	}
+
+    # Sleep for 1 s
+    sleep(1)
