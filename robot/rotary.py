78e6773b602d49da01e74ec4fcd77ad990960ab4
kdrl <admin@kdrl.local>
2016-08-12 08:25:21 +0900

    sample code

--- robot/rotary.py
+++ robot/rotary.py
@@ -7,17 +7,10 @@ knob = grove.GroveRotary(0)
 # Loop indefinitely
 while True:
 
-    # Read values
-    abs = knob.abs_value()
-    absdeg = knob.abs_deg()
-    absrad = knob.abs_rad()
+    # Read values and transform to 0 to 180
+    angle = int(knob.abs_value())*180/1024
 
-    rel = knob.rel_value()
-    reldeg = knob.rel_deg()
-    relrad = knob.rel_rad()
-
-    print "Abs values: %4d" % int(abs) , " raw %4d" % int(absdeg), "deg = %5.2f" % absrad , " rad ",
-    print "Rel values: %4d" % int(rel) , " raw %4d" % int(reldeg), "deg = %5.2f" % relrad , " rad"
+    print "angle: %4d" % angle 
 
     # Sleep for 2.5 s
-    sleep(2.5)
+    sleep(1.5)
