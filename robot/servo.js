b70c91ef8913e16e5b73e1acf5edc76f644d2d9b
kdrl <admin@kdrl.local>
2016-08-12 04:40:55 +0900

    update and fix servo

--- robot/servo.js
+++ robot/servo.js
@@ -2,14 +2,18 @@ var servoModule = require("jsupm_servo");
 
 var servo = new servoModule.ES08A(5);
 
-function setServoAngle(servo, angle, timeOffSet){
+var timeOffSet = 3000;
+
+function setServoAngle(angle, timeOffSet){
 	servo.setAngle(angle);
 	console.log("Set angle to " + angle);
 	var nextAngle = Math.floor(Math.random()*180);
-	setTimeout(setServoAngle(servo, nextAngle, timeOffSet), timeOffSet);
+
+	setTimeout(setServoAngle(nextAngle, timeOffSet), timeOffSet);
 }
 
-setServoAngle(servo, 0, 3000000);
+console.log("start simasu")
+setTimeout(setServoAngle(0, timeOffSet),timeOffSet);
 
 process.on('SIGINT', function()
 {
