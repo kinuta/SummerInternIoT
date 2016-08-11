28d1f4c440a85e56ba814c20dd7ff82751ae1ed7
kdrl <kdrl7@naver.com>
2016-08-11 19:38:01 +0000

    update

--- robot/servo.js
+++ robot/servo.js
@@ -2,14 +2,14 @@ var servoModule = require("jsupm_servo");
 
 var servo = new servoModule.ES08A(5);
 
-function setServoAngel(servo, angle, timeOffSet){
+function setServoAngle(servo, angle, timeOffSet){
 	servo.setAngle(angle);
 	console.log("Set angle to " + angle);
 	var nextAngle = Math.floor(Math.random()*180);
-	setTimeout(setServoAngel(servo, nextAngle), timeOffSet);
+	setTimeout(setServoAngle(servo, nextAngle, timeOffSet), timeOffSet);
 }
 
-startServo(servo, 0, 3000);//初めは0から
+setServoAngle(servo, 0, 3000000);
 
 process.on('SIGINT', function()
 {
