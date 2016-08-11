d912a9334f457c7995a9461ea7e47c1f564dd91b
kdrl <admin@kdrl.local>
2016-08-12 08:28:24 +0900

    sample code

--- robot/rotary.py
+++ robot/rotary.py
@@ -1,16 +1,21 @@
 from time import sleep
 import pyupm_grove as grove
 
-# New knob on AIO pin 0
-knob = grove.GroveRotary(0)
+servo1Rotary = grove.GroveRotary(0)
+servo2Rotary = grove.GroveRotary(1)
+servo3Rotary = grove.GroveRotary(2)
+servo4Rotary = grove.GroveRotary(3)
 
 # Loop indefinitely
 while True:
 
     # Read values and transform to 0 to 180
-    angle = int(knob.abs_value())*180/1024
+    servo1angle = int(servo1Rotary.abs_value())*180/1024
+    servo2angle = int(servo2Rotary.abs_value())*180/1024
+    servo3angle = int(servo3Rotary.abs_value())*180/1024
+    servo4angle = int(servo4Rotary.abs_value())*180/1024
 
-    print "angle: %4d" % angle 
+    print "servo1angle: %4d" % servo1angle, "servo2angle: %4d" % servo2angle, "servo3angle: %4d" % servo3angle, "servo4angle: %4d" % servo4angle 
 
     # Sleep for 2.5 s
     sleep(1.5)
--- robot/servo.py
+++ robot/servo.py
@@ -2,11 +2,11 @@ import time
 import random
 import pyupm_servo as servo 
 
-gServo1 = servo.ES08A(5)#したのやつ
-gServo2 = servo.ES08A(6)
-gServo3 = servo.ES08A(7)
-gServo4 = servo.ES08A(8)
-gServo5 = servo.ES08A(9)#上のやつ。つまりここが手
+servo1 = servo.ES08A(5)#したのやつ
+servo2 = servo.ES08A(6)
+servo3 = servo.ES08A(7)
+servo4 = servo.ES08A(8)
+servo5 = servo.ES08A(9)#上のやつ。つまりここが手
 
 # def setAngle(targetServo, angle):
 #     targetServo.setAngle(angle)
